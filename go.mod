0be96ecf5b59ec3962bc7f49424f19f4aa003288
Yuki's_iMac <s1f101700192@iniad.org>
2021-11-06 13:03:14 +0900

    Oauth2のベース作成

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module iniad.org/IniMaching
 go 1.16
 
 require (
+	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
 	github.com/golang/glog v1.0.0 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/zalando/gin-oauth2 v1.5.3 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zalando/gin-oauth2/google"
 )
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+
+	//URL コントローラー的な
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
 		})
 	})
-	router.Run(":8000")
+
+	router.GET("/login", google.LoginHandler)
+
+	private := router.Group("/auth")
+	private.Use(google.Auth())
+	private.GET("/MyPage", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	//GoogleLogin
+	redirectURL := "https://inimaching.herokuapp.com/auth"
+	credFile := os.Getenv("credFile") //これだとバグるからファイルじゃなくてオブジェクトから読み込む方法模索する
+	scopes := []string{
+		"https://www.googleapis.com/auth/userinfo.email",
+	}
+	secret := []byte("secret")
+	sessionName := "goquestsession"
+	google.Setup(redirectURL, credFile, scopes, secret)
+	router.Use(google.Session(sessionName))
+
+	port := os.Getenv("PORT")
+	router.Run(port)
 }
